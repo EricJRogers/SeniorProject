66536878c48e7176f1cb5ca4e511ecc7ba8df0df
Eric Rogers <ericjrogers18@gmail.com>
2016-11-09 10:05:36 -0500

    update

--- Assets/Scripts/index.js
+++ Assets/Scripts/index.js
@@ -1,13 +1,20 @@
 function loadModal() {
   //Build Modal_ID from clicked Button_ID
   var modal_id = '#modal-'.concat((this.id).substring(7,8));
-  
+
   //Load proper Modal based on clicked button
   $(modal_id).modal();
 };
 
+function gitWin() {
+  console.log("gitWin");
+  $('.terminal-window').append("git install\nuser@terminal:~$ ");
+}
+
 function init() {
+  //Button clicked will Load Modal
   $('.btn').click(loadModal);
+  $('#gitWin').click(gitWin);
 };
 
 $(document).ready(init);
