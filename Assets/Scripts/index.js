248359625e0a8f693d440b81c999595ff276e679
Eric Rogers <ericjrogers18@gmail.com>
2016-12-19 09:54:24 -0500

    terminal window operations finished

--- Assets/Scripts/index.js
+++ Assets/Scripts/index.js
@@ -1,5 +1,9 @@
+//Global variables
+var typeTimer;
+
 //Function will find and load modal based on the id of the button clicked
 function loadModal() {
+//  clearTerminal;
   var button_id = this.id;
   var modal_id = '#modal-'.concat(button_id.substring(7));
   $(modal_id).modal();
@@ -8,26 +12,67 @@ function loadModal() {
   }
 };
 
-//Function displays proper instructions to modal terminal display
-function gitInstall() {
-  typeText('.terminal-window', 'sudo apt-get install git-all', 0, 200);
+//Function displays proper instructions to modal terminal display based on id of modal-btn clicked
+function generateInstruction() {
+  if(typeTimer != null) {
+    clearTimeout(typeTimer);
+    $('.terminal-window').append('\nuser@terminal:~$ ');
+  }
+
+  switch(this.id) {
+    //Begin modal-1 instructions
+    case "gitWin":
+      typeText('.terminal-window', 'sudo apt-get install windows', 0, 200);
+      break;
+    case "gitLin":
+      typeText('.terminal-window', 'sudo apt-get install git-all', 0, 200);
+      break;
+    case "gitOSX":
+      typeText('.terminal-window', 'sudo apt-get install osx', 0, 200);
+      break;
+    case "gitClone":
+      typeText('.terminal-window', 'git clone "repository link"', 0, 200);
+      break;
+    //Begin modal-2 instructions
+    //Begin modal-3 instructions
+    //Begin modal-4 instructions
+    //Begin modal-5 instructions
+    //Begin modal-6 instructions
+    //Begin modal-7 instructions
+    //Begin modal-8 instructions
+    //Begin modal-9 instructions
+    default:
+      typeText('.terminal-window', 'no instruction found', 0, 200);
+  }
 }
 
 //Function will type one letter at a time of 'string' to the 'target' element over a give 'interval'
 function typeText(target, string, index, interval) {
   if (index < string.length) {
     $(target).append(string[index++]);
-    setTimeout(function () { typeText(target, string, index, interval); }, interval);
+    typeTimer = setTimeout(function () { typeText(target, string, index, interval); }, interval);
   }
-  else
+  else {
     $(target).append('\nuser@terminal:~$ ');
+    typeTimer = null;
+  }
+}
+
+//Clears terminal text on Modal close
+function clearTerminal() {
+  clearTimeout(typeTimer);
+  $('.terminal-window').empty();
+  $('.terminal-window').append('user@terminal:~$ ');
 }
 
 //Functions listening once document is loaded
 function init() {
   //Button clicked will Load Modal
   $('.main-btn').click(loadModal);
-  $('#gitLin').click(gitInstall);
+  //Modal-btn clicked will generate/display proper instructions
+  $('.modal-btn').click(generateInstruction);
+  //Will clear terminal window when modal becomes hidden
+  $('.modal').on('hidden.bs.modal', clearTerminal);
 };
 
 $(document).ready(init);
